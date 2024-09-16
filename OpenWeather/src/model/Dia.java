427d7043a96526c48f87317e22e115b776857b6d
ManuNovillo <manuelnovilloinformatica@gmail.com>
2024-09-16 20:59:45 +0200

    parece que funciona

---
+++ OpenWeather/src/model/Dia.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Dia {
+	String timeFrom;
+	String timeTo;
+	String symbolName;
+	String precipitationProb;
+	String tempMax;
+	String tempMin;
+	public Dia(String timeFrom, String timeTo, String symbolName, String precipitationProb, String tempMax,
+			String tempMin) {
+		this.timeFrom = timeFrom;
+		this.timeTo = timeTo;
+		this.symbolName = symbolName;
+		this.precipitationProb = precipitationProb;
+		this.tempMax = tempMax;
+		this.tempMin = tempMin;
+	}
+	
+	public String getTimeFrom() {
+		return timeFrom;
+	}
+	
+	public String getTimeTo() {
+		return timeTo;
+	}
+	
+	public String getSymbolName() {
+		return symbolName;
+	}
+	
+	public String getPrecipitationProb() {
+		return precipitationProb;
+	}
+	
+	public String getTempMax() {
+		return tempMax;
+	}
+	
+	public String getTempMin() {
+		return tempMin;
+	}
+
+	@Override
+	public String toString() {
+		return "Dia [timeFrom=" + timeFrom + ", timeTo=" + timeTo + ", symbolName=" + symbolName
+				+ ", precipitationProb=" + precipitationProb + ", tempMax=" + tempMax + ", tempMin=" + tempMin + "]";
+	}
+	
+}
