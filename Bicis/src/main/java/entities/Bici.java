3a8df8f6acebe59b79bc2e97e421199a64df4b7e
Manuel <manuelnovilloinformatica@gmail.com>
2024-11-04 14:13:03 +0100

    clase

--- Bicis/src/main/java/entities/Bici.java
+++ Bicis/src/main/java/entities/Bici.java
@@ -1,54 +1,60 @@
 package entities;
 
-import dao.PrimaryKey;
-
 public class Bici {
-	
-	@PrimaryKey
+
 	private int id;
 	private int fav;
 	private double precio;
 	private String foto;
 	private String descripcion;
-	@EntityId(className = "entities.Marca")
-	private Marca marca;
-	
+	private int marca;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getFav() {
 		return fav;
 	}
+
 	public void setFav(int fav) {
 		this.fav = fav;
 	}
+
 	public double getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
+
 	public String getFoto() {
 		return foto;
 	}
+
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	public Marca getMarca() {
+
+	public int getMarca() {
 		return marca;
 	}
-	public void setMarca(Marca marca) {
+
+	public void setMarca(int marca) {
 		this.marca = marca;
 	}
-	
-	
+
 }
