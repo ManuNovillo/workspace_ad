ca76fdb481697ff0c634a0ea8b193e0a8560a826
Manuel <manuelnovilloinformatica@gmail.com>
2025-02-07 14:11:55 +0100

    clase

--- tmdb/src/main/java/util/JPAUtil.java
+++ tmdb/src/main/java/util/JPAUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
     private static final EntityManagerFactory emf;
-    static{
+    static {
         try{
             emf=Persistence.createEntityManagerFactory("tmdbPU");
         } catch (Throwable ex) {
