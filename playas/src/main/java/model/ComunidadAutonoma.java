596ff437f239c9f622735610735f234ea673464d
ManuNovillo <manuelnovilloinformatica@gmail.com>
2025-02-16 20:43:07 +0100

    casa

---
+++ playas/src/main/java/model/ComunidadAutonoma.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CCAA")
+public class ComunidadAutonoma {
+
+    @Id
+    @
+}
